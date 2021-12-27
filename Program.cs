f5827c9028a2d74a5135d57d14c71146f6c63244
stevegee58 <stevegee58@gmail.com>
2021-12-27 14:35:32 -0500

    Added games table to db with bot/human counts Replaced kills with bot/human kill counts to players table

--- Program.cs
+++ Program.cs
@@ -10,15 +10,17 @@ using FortniteReplayReader.Models.Events;
 using System.Linq;
 using System.Data.SQLite;
 
-
 namespace ConsoleReader
 {
     using PlatformCountsType = System.Collections.Generic.Dictionary<string, int>;
     using GUIDCountsType = System.Collections.Generic.Dictionary<string, int>;
+    using ElimCountsType = System.Collections.Generic.Dictionary<string, int>;
 
     class Program
     {
         static PlatformCountsType platformCounts = new PlatformCountsType();
+        static ElimCountsType botElims = new ElimCountsType();
+        static ElimCountsType humanElims = new ElimCountsType();
 
         static void UpdatePlatformCount(FortniteReplayReader.Models.PlayerData item)
         {
@@ -46,18 +48,55 @@ namespace ConsoleReader
             }
         }
 
+        static void UpdateElimCounts(FortniteReplayReader.Models.KillFeedEntry item)
+        {
+            if (!item.FinisherOrDownerIsBot && item.FinisherOrDownerName != null)
+            {
+                if (item.PlayerIsBot)
+                {
+                    if (item.FinisherOrDownerName != null)
+                    {
+
+                    }
+                    if (botElims.ContainsKey(item.FinisherOrDownerName))
+                    {
+                        botElims[item.FinisherOrDownerName]++;
+                    }
+                    else
+                    {
+                        botElims[item.FinisherOrDownerName] = 1;
+                    }
+                }
+                else
+                {
+                    if (humanElims.ContainsKey(item.FinisherOrDownerName))
+                    {
+                        humanElims[item.FinisherOrDownerName]++;
+                    }
+                    else
+                    {
+                        humanElims[item.FinisherOrDownerName] = 1;
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            string cs = @"URI=file:players.sqlite";
+            string cs = @"URI=file:fortnite_game_stats.sqlite";
             using var con = new SQLiteConnection(cs);
             con.Open();
 
             using var cmd = new SQLiteCommand(con);
 
+#if false
             cmd.CommandText = "DROP TABLE IF EXISTS players";
             cmd.ExecuteNonQuery();
+#endif
+            cmd.CommandText = @"CREATE TABLE IF NOT EXISTS players(timestamp TEXT, guid TEXT, name TEXT, placed INT, bot_kills INT, human_kills INT, platform TEXT, killed_by TEXT, PRIMARY KEY (timestamp, guid))";
+            cmd.ExecuteNonQuery();
 
-            cmd.CommandText = cmd.CommandText = @"CREATE TABLE players(timestamp TEXT, guid TEXT, name TEXT, placed INT, kills INT, platform TEXT, killed_by TEXT, PRIMARY KEY (timestamp, guid))";
+            cmd.CommandText = @"CREATE TABLE IF NOT EXISTS games(timestamp TEXT, humans INT, bots INT, henchmen INT, PRIMARY KEY (timestamp))";
             cmd.ExecuteNonQuery();
 
             string replayFilesFolder;
@@ -94,8 +133,8 @@ namespace ConsoleReader
                 var humanMap = new Dictionary<string, string>();
                 float[] deathTimes = new float[100];
                 int henchmenCount = 0;
-                int myHumanElims = 0;
-                int myBotElims = 0;
+                botElims.Clear();
+                humanElims.Clear();
                 string killedBy = "******You Won******";
                 var sw = new Stopwatch();
                 sw.Start();
@@ -176,7 +215,7 @@ namespace ConsoleReader
                                 kills = (int)item.Kills;
                             }
 
-                            cmd.CommandText = $"INSERT INTO players(name, guid, timestamp, placed, kills, platform) VALUES('{item.PlayerName}','{item.EpicId}','{timestamp}',{placement},{kills},'{item.Platform}')";
+                            cmd.CommandText = $"INSERT OR REPLACE INTO players(name, guid, timestamp, placed, bot_kills, human_kills, platform) VALUES('{item.PlayerName}','{item.EpicId}','{timestamp}',{placement},{0},{0},'{item.Platform}')";
                             cmd.ExecuteNonQuery();
                         }
                         else if (item.BotId != null)
@@ -229,16 +268,16 @@ namespace ConsoleReader
 
                     foreach (FortniteReplayReader.Models.KillFeedEntry item in replay.KillFeed)
                     {
+                        UpdateElimCounts(item);
+
                         if (item.FinisherOrDownerName == myGUID)
                         {
                             //Console.WriteLine($"YOU killed {item.PlayerName} (bot: {item.PlayerIsBot})");
                             if (item.PlayerIsBot)
                             {
-                                myBotElims++;
                             }
                             else
                             {
-                                myHumanElims++;
                                 myElimGUIDs[item.PlayerName] = humanMap[item.PlayerName];
                             }
                         }
@@ -271,6 +310,32 @@ namespace ConsoleReader
                         }
                     }
 
+                    foreach(var human in humanMap)
+                    {
+                        int kills = 0;
+                        if (botElims.ContainsKey(human.Key))
+                        {
+                            kills = botElims[human.Key];
+                        }
+                        else
+                        {
+                            kills = 0;
+                        }
+                        cmd.CommandText = $"UPDATE players SET bot_kills = '{kills}' WHERE guid = '{human.Key}' AND timestamp = '{timestamp}'";
+                        cmd.ExecuteNonQuery();
+
+                        if (humanElims.ContainsKey(human.Key))
+                        {
+                            kills = humanElims[human.Key];
+                        }
+                        else
+                        {
+                            kills = 0;
+                        }
+                        cmd.CommandText = $"UPDATE players SET human_kills = '{kills}' WHERE guid = '{human.Key}' AND timestamp = '{timestamp}'";
+                        cmd.ExecuteNonQuery();
+                    }
+
                     string lineFormat = "|{0,4}|{1,41}|{2,6}|{3,4}|{4,8}|{5,11}|{6,6}|{7,5}|{8,9}|{9,11}|{10,32} |";
                     if (gameNumber == 0)
                     {
@@ -291,8 +356,21 @@ namespace ConsoleReader
                             killedBy = "******You Died******";
                         }
                     }
+                    int myBotElims = 0;
+                    int myHumanElims = 0;
+                    if(botElims.ContainsKey(myGUID))
+                    {
+                        myBotElims = botElims[myGUID];
+                    }
+                    if (humanElims.ContainsKey(myGUID))
+                    {
+                        myHumanElims = humanElims[myGUID];
+                    }
                     Console.WriteLine(lineFormat,
                         gameNumber, Path.GetFileName(replayFile), humanMap.Count, botMap.Count, henchmenCount, (humanMap.Count + botMap.Count), position, totalPlayers, myBotElims, myHumanElims, killedBy);
+
+                    cmd.CommandText = $"INSERT OR REPLACE INTO games(timestamp, humans, bots, henchmen) VALUES('{timestamp}', {humanMap.Count()}, {botMap.Count()}, {henchmenCount})";
+                    cmd.ExecuteNonQuery();
                 }
                 catch (Exception ex)
                 {
